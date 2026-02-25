3a15686afa2c01edbca96eb4bb10030ad77744b0
Carl <carlsagan77@gmail.com>
2026-02-24 23:23:12 -0300

    Primeiro Commit. Feita estrutura do projeto e criado camadas, também foram criado objetos para serem futuramente usados.

---
+++ Lib/VO/AtividadeVO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.VO
+{
+    public class AtividadeVO
+    {
+        public int Codigo { get; set; }
+
+        public string Nome { get; set; } = string.Empty;
+
+        public int CodigoCategoria { get; set; }
+
+        public DateTime DataAtividade { get; set; }
+
+        public DateTime? DataInicio { get; set; }
+
+        public DateTime? DataConclusao { get; set; }
+
+        public int TempoEstimado { get; set; }
+        public int TempoConcluido { get; set; }
+
+        public bool Concluida { get; set; }
+
+        public string Observacoes { get; set; } = string.Empty;
+        public double PercentualConcluido
+        {
+            get
+            {
+                if (TempoEstimado == 0) return 0;
+                return Math.Round((double)TempoConcluido / TempoEstimado * 100, 2);
+            }
+        }
+    }
+}
---
+++ Lib/VO/SessaoAtividadeVO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyTracker.Lib.VO;
+
+public class SessaoAtividadeVO
+{
+    public int Codigo { get; set; }
+
+    public int CodigoAtividade { get; set; }
+
+    public DateTime HoraInicio { get; set; }
+
+    public DateTime HoraFim { get; set; }
+
+    public int DuracaoMinutos 
+        => (int)(HoraFim - HoraInicio).TotalMinutes;
+}
